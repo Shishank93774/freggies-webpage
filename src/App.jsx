a70960fb0bb13e41510c8ef842bc7ab4c9f6d43f
Shishank <shishankrawat93774@gmail.com>
2023-08-14 22:51:42 +0530

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,62 @@ import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/navbar';
 import Sliders from './components/sliders/Sliders';
 
+const cardsData = [
+  {
+    label: "Handbag",
+    alt: "image3",
+    url: "https://static.wixstatic.com/media/22e53e_efc1552d8050407f82ea158302d0debd~mv2.jpg/v1/fill/w_663,h_663,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/22e53e_efc1552d8050407f82ea158302d0debd~mv2.jpg",
+    price: "499"
+  },
+  {
+    label: "Sweater",
+    alt: "image4",
+    url: "https://static.wixstatic.com/media/22e53e_01575d792adb43a6a16595bd74a1cc8d~mv2.jpg/v1/fill/w_663,h_663,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/22e53e_01575d792adb43a6a16595bd74a1cc8d~mv2.jpg",
+    price: "2,099"
+  },
+  {
+    label: "Flower Vase",
+    alt: "image5",
+    url: "https://static.wixstatic.com/media/22e53e_2fee033b2eca46cab4eec7fa74e99c31~mv2.jpg/v1/fill/w_663,h_663,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/22e53e_2fee033b2eca46cab4eec7fa74e99c31~mv2.jpg",
+    price: "1,099"
+  },
+  {
+    label: "T-Shirt",
+    alt: "image6",
+    url: "https://static.wixstatic.com/media/22e53e_8adb0d7018b047e0a998acf987fd3fd6~mv2.jpg/v1/fill/w_663,h_663,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/22e53e_8adb0d7018b047e0a998acf987fd3fd6~mv2.jpg",
+    price: "449"
+  },
+]
+;
+const cardsData2 = [
+  {
+    label: "Handbag",
+    alt: "image3",
+    url: "https://static.wixstatic.com/media/22e53e_efc1552d8050407f82ea158302d0debd~mv2.jpg/v1/fill/w_663,h_663,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/22e53e_efc1552d8050407f82ea158302d0debd~mv2.jpg",
+    price: "499"
+  },
+  {
+    label: "Sweater",
+    alt: "image4",
+    url: "https://static.wixstatic.com/media/22e53e_01575d792adb43a6a16595bd74a1cc8d~mv2.jpg/v1/fill/w_663,h_663,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/22e53e_01575d792adb43a6a16595bd74a1cc8d~mv2.jpg",
+    price: "2,099"
+  },
+  {
+    label: "T-Shirt",
+    alt: "image6",
+    url: "https://static.wixstatic.com/media/22e53e_8adb0d7018b047e0a998acf987fd3fd6~mv2.jpg/v1/fill/w_663,h_663,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/22e53e_8adb0d7018b047e0a998acf987fd3fd6~mv2.jpg",
+    price: "449"
+  },
+];
+
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Hero></Hero>
-      <Sliders title={"BEST SELLERS"}></Sliders>
-      <Sliders title={"BEST OFFERS"}></Sliders>
-      <Sliders title={"KITCHEN MUST HAVE's"}></Sliders>
+      <Sliders data={cardsData} title={"BEST SELLERS"}></Sliders>
+      <Sliders data={cardsData2} title={"BEST OFFERS"}></Sliders>
+      <Sliders data={cardsData} title={"KITCHEN MUST HAVE's"}></Sliders>
       <Events></Events>
       <Contact></Contact>
       <Footer></Footer>
--- src/components/sliders/Sliders.jsx
+++ src/components/sliders/Sliders.jsx
@@ -1,34 +1,8 @@
 import "./Sliders.css";
 import { useState } from "react";
 
-const cardsData = [
-  {
-    label: "Handbag",
-    alt: "image3",
-    url: "https://static.wixstatic.com/media/22e53e_efc1552d8050407f82ea158302d0debd~mv2.jpg/v1/fill/w_663,h_663,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/22e53e_efc1552d8050407f82ea158302d0debd~mv2.jpg",
-    price: "499"
-  },
-  {
-    label: "Sweater",
-    alt: "image4",
-    url: "https://static.wixstatic.com/media/22e53e_01575d792adb43a6a16595bd74a1cc8d~mv2.jpg/v1/fill/w_663,h_663,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/22e53e_01575d792adb43a6a16595bd74a1cc8d~mv2.jpg",
-    price: "2,099"
-  },
-  {
-    label: "Flower Vase",
-    alt: "image5",
-    url: "https://static.wixstatic.com/media/22e53e_2fee033b2eca46cab4eec7fa74e99c31~mv2.jpg/v1/fill/w_663,h_663,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/22e53e_2fee033b2eca46cab4eec7fa74e99c31~mv2.jpg",
-    price: "1,099"
-  },
-  {
-    label: "T-Shirt",
-    alt: "image6",
-    url: "https://static.wixstatic.com/media/22e53e_8adb0d7018b047e0a998acf987fd3fd6~mv2.jpg/v1/fill/w_663,h_663,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/22e53e_8adb0d7018b047e0a998acf987fd3fd6~mv2.jpg",
-    price: "449"
-  },
-];
 
-const Sliders = ({ title }) => {
+const Sliders = ({ title, data }) => {
   // const [currentFirstIndex, setCurrentFirstIndex] = useState(0);
   // function rotateCards() {
   //   setCurrentFirstIndex(currentFirstIndex+1);
@@ -37,7 +11,7 @@ const Sliders = ({ title }) => {
     <div className="slider-container">
       <div className="slider-title">{title}</div>
       <div className="cards-container">
-        {cardsData.map((card) => {
+        {data.map((card) => {
           return (
             <div className="card" key={card}>
               <div className="card-img-container">
